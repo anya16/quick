f98752c55bf63c18009028b6cbd1634198f23caa
sky <1123978215@qq.com>
2017-02-19 21:46:01 +0800

    update: es6 transform

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import { controllerDetail } from './detail';
 import $ from 'jquery';
 $(function() {
-	console.log('9099');
 	controllerDetail();
 });
--- src/detail/index.js
+++ src/detail/index.js
@@ -1,5 +1,13 @@
-require('./index.scss');
-var controllerDetail = function () {
-   console.log('de366');
+require('./detail.scss');
+import { BaseModel } from '../model/baseModel';
+var detailModel = {
+    name: 'Detail',
+    des: 'Model of Detail',
+    date: '2017/02/19'
 };
-export {controllerDetail};
+var controllerDetail = function() {
+    let baseModel = new BaseModel(detailModel);
+    let result = baseModel.toString();
+    console.log(result);
+};
+export { controllerDetail };
