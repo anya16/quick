5524223dfc1930e937df3802761ba0609a4768a7
sky <1123978215@qq.com>
2017-02-19 23:32:46 +0800

    add:directorjs 实现路由

--- src/app.js
+++ src/app.js
@@ -1,5 +1,14 @@
 import { controllerDetail } from './detail';
 import $ from 'jquery';
+import D from 'director/build/director';
+var Router = D.Router;
 $(function() {
-	controllerDetail();
+    let routes = {
+        '/detail': controllerDetail,
+        '/list': () => {
+            console.log('list');
+        }
+    };
+    let router = Router(routes);
+    router.init();
 });
