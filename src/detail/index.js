9da9f6924b9d32d4a550b1a3e451b9de5bc73015
sky <1123978215@qq.com>
2017-02-20 23:59:11 +0800

    add:实现渲染

--- src/detail/index.js
+++ src/detail/index.js
@@ -1,19 +1,40 @@
-require('./detail.scss');
 import { BaseModel } from '../model/baseModel';
 import { BaseView } from '../view/baseView';
+import $ from 'jquery';
+require('./detail.scss');
+var tpl = require('./detail.html');
 var detailModel = {
-    name: 'Detail',
-    des: 'Model of Detail',
-    date: '2017/02/19'
+    body: [{
+        name: 'Detail1',
+        des: 'Model of Detail',
+        date: '2017/02/01'
+    }, {
+        name: 'Detail2',
+        des: 'Model of Detail',
+        date: '2017/02/02'
+    }, {
+        name: 'Detail3',
+        des: 'Model of Detail',
+        date: '2017/02/02'
+    }, {
+        name: 'Detail4',
+        des: 'Model of Detail',
+        date: '2017/02/03'
+    }]
 };
 
-class controllerDetail {
+class ControllerDetail {
     constructor() {
-        let baseModel = new BaseModel(detailModel);
-        let result = baseModel.toString();
-        console.log(result);
+        this.shut();
+        let param = {};
+        let result = new BaseModel(detailModel).toString();
+        param.template = tpl;
+        param.model = result;
+        let baseView = new BaseView(param);
     }
+    shut() {
+        $('view').hide();
+    }
+};
 
-}
-
-export { controllerDetail };
+export { ControllerDetail };
--- src/view/baseView.js
+++ src/view/baseView.js
@@ -1,9 +1,25 @@
+import _ from 'underscore';
+import $ from 'jquery';
 class BaseView {
-    constructor(model) {
-    	this.model = model
+    constructor(param) {
+        this.$app = $('#app');
+        this.$ele = $('<div id="detail" class="view">');
+        this.model = param.model;
+        this.template = param.template;
+        this.filter();
+        this.render();
     }
     render() {
-    	console.log(this.model);
+        let tpl = this.template;
+        let model = this.model;
+        let html = _.template(tpl)(model);
+        this.$ele.html(html);
+        this.$app.append(this.$ele);
+    }
+    filter() {
+        let _tpl = this.template;
+        _tpl.indexOf('module.exports = ') == 0 ? _tpl = _tpl.replace('module.exports = ', '') : '';
+        this.template = _tpl;
     }
 }
 export { BaseView };
